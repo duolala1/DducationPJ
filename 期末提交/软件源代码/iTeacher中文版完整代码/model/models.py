27ad8ff6887b40a57ef49b9e596e3279b6b4b29a
mirrorssssssss <mirrorssssssss@outlook.com>
2017-06-13 23:52:08 +0800

    model

---
+++ 期末提交/软件源代码/iTeacher中文版完整代码/model/models.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class Test(models.Model):
+    name = models.CharField(max_length=20)
+class bookInfo(models.Model):
+	bookID = models.CharField(max_length=50)
+	bookName = models.CharField(max_length=300)
+	bookDetail = models.CharField(max_length=3000)
+	imgUrl = models.CharField(max_length=500)
+	author = models.CharField(max_length=100)
+	bookIntro = models.CharField(max_length=300,default='')
+class labels(models.Model):
+	bookID = models.CharField(max_length=50)
+	label = models.CharField(max_length=200)
+
+class user(models.Model):
+	userName = models.CharField(max_length=50)
+	userPasswd = models.CharField(max_length=50)
+class Preference(models.Model):
+	userName = models.CharField(max_length=50)
+	comments = models.CharField(max_length=500)
+	bookID = models.CharField(max_length=50)
+class scores(models.Model):
+	userName = models.CharField(max_length=50)
+	score = models.CharField(max_length=3)
+	bookID = models.CharField(max_length=50)
+class History(models.Model):
+	userName = models.CharField(max_length=50)
+	bookID = models.CharField(max_length=50)
+class Collection(models.Model):
+	userName = models.CharField(max_length=50)
+	bookID = models.CharField(max_length=50)
+class Recommend(models.Model):
+	userName = models.CharField(max_length=50)
+	bookID = models.CharField(max_length=50)
+class Interests(models.Model):
+	userName = models.CharField(max_length=50)
+	label = models.CharField(max_length=50)
